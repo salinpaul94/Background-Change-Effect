85b4a0738597b65be81b16fee34acacb3711e172
salinpaul valooran <salinpv@hotmail.com>
2023-11-11 01:09:25 +0530

    fixing original image height and change only in img width

--- version_01/index.js
+++ version_01/index.js
@@ -3,6 +3,8 @@ let ImgWrap = document.querySelector(".img-wrap");
 let leftSpace = ImgBox.offsetLeft;
 let originalImg = document.getElementById("originalImg");
 
+originalImg.style.width = ImgBox.offsetWidth + "px";
+
 ImgBox.onmousemove = function (e) {
   let boxWidth = e.pageX - leftSpace + "px";
   ImgWrap.style.width = boxWidth;
