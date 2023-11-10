dd8d46ee60df282f66f57ff29917c370791fda6f
salinpaul valooran <salinpv@hotmail.com>
2023-11-11 00:47:57 +0530

    variable for getting left space of .img-box

--- version_01/index.js
+++ version_01/index.js
@@ -1,6 +1,6 @@
 let ImgBox = document.querySelector(".img-box");
 let ImgWrap = document.querySelector(".img-wrap");
-
+let leftSpace = ImgBox.offsetLeft;
 
 ImgBox.onmousemove = function(e) {
   e.pageX;
