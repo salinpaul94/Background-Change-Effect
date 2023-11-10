48d4c6829a6ff76f4425e4812c89f6c975d1ccec
salinpaul valooran <salinpv@hotmail.com>
2023-11-11 00:54:20 +0530

    variable for original img

--- version_01/index.js
+++ version_01/index.js
@@ -1,6 +1,7 @@
 let ImgBox = document.querySelector(".img-box");
 let ImgWrap = document.querySelector(".img-wrap");
 let leftSpace = ImgBox.offsetLeft;
+let originalImg = document.getElementById("originalImg");
 
 ImgBox.onmousemove = function (e) {
   let boxWidth = e.pageX - leftSpace + "px";
