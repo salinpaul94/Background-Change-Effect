76a4db997c6ed47e97fd97b7c7649bfaec0c8236
salinpaul valooran <salinpv@hotmail.com>
2023-11-11 00:49:25 +0530

    getting current position of the mouse in px

--- version_01/index.js
+++ version_01/index.js
@@ -2,6 +2,6 @@ let ImgBox = document.querySelector(".img-box");
 let ImgWrap = document.querySelector(".img-wrap");
 let leftSpace = ImgBox.offsetLeft;
 
-ImgBox.onmousemove = function(e) {
-  e.pageX;
-}
+ImgBox.onmousemove = function (e) {
+  let boxWidth = e.pageX - leftSpace + "px";
+};
